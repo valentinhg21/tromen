6e8c727bd36c8f6834f80e0edd932db53d80a03b
Valentin Gutierrez <valentin@zetenta.com>
2025-11-11 11:02:20 -0300

    Ajustes de class requerid

--- class/requerid-plugins.php
+++ class/requerid-plugins.php
@@ -154,8 +154,8 @@ function zetenta_register_required_plugins() {
 		'id'           => 'zetenta',                 // Unique ID for hashing notices for multiple instances of TGMPA.
 		'default_path' => '',                      // Default absolute path to bundled plugins.
 		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'has_notices'  => false,                    // Show admin notices or not.
+		'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
 		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
 		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
 		'message'      => '',                      // Message to output right before the plugins table.
